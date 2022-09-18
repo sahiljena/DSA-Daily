8aca99b12baffeac715907a8d26836211539c7b0
Sahil Jena <sahiljena@protonmail.com>
2022-09-18 12:49:32 +0530

    Time: 66 ms (42.86%), Space: 37.3 MB (14.29%) - LeetHub

---
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        if(s.length()==1) return 1;
+        int ans = 0;
+        vector<int> arr;
+        for(char i:s) arr.push_back(i-'a');
+        int cnt=1;
+        for(int i=1;i<s.length();i++){
+            //cout<<arr[i-1]<<" ";
+            if(arr[i-1]+1==arr[i]){
+                cnt+=1;
+                ans=max(ans,cnt);   
+            }else cnt=1;
+        }
+        return max(cnt,ans);
+    }
+};
