33c2ee17f89abc807bbed410aa055d3317b69da1
Sahil Jena <sahiljena@protonmail.com>
2022-09-18 12:52:25 +0530

    Time: 144 ms (28.57%), Space: 37.5 MB (14.29%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int longestContinuousSubstring(string s) {
-        if(s.length()==1) return 1;
-        int ans = 0;
+        int ans = 0, cnt=1;
         vector<int> arr;
         for(char i:s) arr.push_back(i-'a');
-        int cnt=1;
         for(int i=1;i<s.length();i++){
             if(arr[i-1]+1==arr[i]){
                 cnt+=1;
                 ans=max(ans,cnt);   
             }else cnt=1;
         }
-        return ans==0?1:ans;
+        return ans?ans:1;
     }
 };
