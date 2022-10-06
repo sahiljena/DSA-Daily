329a0abae7941ec1452e6bcd2d541f214d6e7893
Sahil Jena <sahiljena@protonmail.com>
2022-10-06 23:59:05 +0530

    Time: 367 ms (36.93%), Space: 62.5 MB (10.25%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
+    map<pair<int,int>,int> dp;
     int findWays(vector<int>&nums, int target, int curr, int index){
         if(index==nums.size()){
             if(curr==target) return 1;
             return 0;
         }
-        int tk = findWays(nums, target, curr+nums[index], index+1);
-        int ntk = findWays(nums, target, curr-nums[index], index+1);
-        return tk+ntk;
+        if(dp.find({curr, index})!=dp.end()) return dp[{curr, index}];
+        int take = findWays(nums, target, curr+nums[index], index+1);
+        int notake = findWays(nums, target, curr-nums[index], index+1);
+        return dp[{curr, index}] = take+notake;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         return findWays(nums, target, 0, 0);
