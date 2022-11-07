b68830ca865b89b38df0d2859de22e384baf33c6
Sahil Jena <sahiljena@protonmail.com>
2022-11-07 23:50:35 +0530

    Time: 6 ms (9.01%), Space: 5.8 MB (82.75%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        
+        string t = to_string(num);
+        for(int i=0;i<t.length();i++){
+            if(t[i]=='6'){
+                t[i]='9';
+                break;
+            }
+        }
+        return stoi(t);
+        
+    }
+};
