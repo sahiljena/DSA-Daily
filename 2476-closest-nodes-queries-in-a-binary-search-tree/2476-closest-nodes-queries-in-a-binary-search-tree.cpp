74f007402fc88cd8409374c77dae6273fcf6b39f
Sahil Jena <sahiljena@protonmail.com>
2022-11-20 12:50:03 +0530

    Time: 804 ms (11.11%), Space: 132.9 MB (33.33%) - LeetHub

--- 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -12,23 +12,16 @@
 class Solution {
 public:
     vector<int> treeArr;
-    //map<int,int>mp;
     void helper(TreeNode* root){
         if(!root) return;
         helper(root->left);
         treeArr.push_back(root->val);
-        //mp[root->val]++;
         helper(root->right);
     }
     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
         helper(root);
         vector<vector<int>> ans;
         for(int i:queries){
-            // if(mp[i]>=1){ 
-            //     ans.push_back({i,i});
-            //     continue;
-            // }
-            
             int minDiff=INT_MAX;
             int maxDiff=INT_MAX;
             int minElem=-1, maxElem=-1;
@@ -46,16 +39,8 @@ public:
                         maxDiff=treeArr[mid]-i;
                         maxElem=treeArr[mid];
                     }
-                    // if(treeArr[mid-1]<i && mid-1>=0 && i-treeArr[mid-1]<minDiff){
-                    //     minDiff=treeArr[mid-1]-i;
-                    //     minElem=treeArr[mid-1];
-                    // }
                     high=mid-1;
                 }else{
-                    // if(treeArr[mid+1]>i && treeArr[mid]-i<maxDiff){
-                    //     maxDiff=treeArr[mid]-i;
-                    //     maxElem=treeArr[mid];
-                    // }
                     if(i-treeArr[mid]<minDiff){
                         minDiff=i-treeArr[mid];
                         minElem=treeArr[mid];
